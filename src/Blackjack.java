be1faf78b61a497bc61163d7b308c52bdb323436
tomekstolarczyk <tomasz.d.stolarczyk@gmail.com>
2025-10-17 16:34:35 +0200

    building card deck

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -6,5 +6,43 @@ import javax.swing.*;
 
 public class Blackjack {
 
+    private class Card {
 
+        String type;
+        String value;
+
+        Card(String type, String value) {
+            this.type = type;
+            this.value = value;
+        }
+    }
+
+    
+
+    Blackjack() {
+        startGame();
+    }
+
+    void startGame() {
+      //deck
+      buildDeck();
+
+    }
+
+    ArrayList<Card> deck;
+    
+    public void buildDeck() {
+
+        deck = new ArrayList<Card>();
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        String[] types = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        for (String value : values) {
+            for (String type : types) {
+                Card card = new Card(type, value);
+                deck.add(card);
+            }
+        }
+        System.out.println("Building Deck");
+        System.out.println(deck);
+    }
 }
